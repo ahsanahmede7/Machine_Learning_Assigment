4a8d6a34d52908c648da306fed3dc8d5981551ac
Ahsanahmede7 <ahsansheikhe7@gmail.com>
2026-05-25 15:50:14 +0500

    Update

--- Backend/API_Call.py
+++ Backend/API_Call.py
@@ -1,32 +1,35 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 import pickle
 import pandas as pd
-with open('model.pkl', 'rb') as f:
+with open('Decision_tree_model.pkl', 'rb') as f:
     model = pickle.load(f)
-app = FastAPI()
+# app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {'message': "Hello Welcome to Our Machine Learning Model"}
+# @app.get("/")
+# def read_root():
+#     return {'message': "Hello Welcome to Our Machine Learning Model"}
 
-@app.post("/predict")
-def predict(age:int,workclass:str,fnlwgt:int,education:str,educational_num:int,marital_status:str,occupation:str,relationship:str,race:str,gender:str,capital_gain:int,capital_loss:int,hours_per_week:int,native_country:str):
-    data = pd.DataFrame({
-        'age': [age],
-        'workclass': [workclass],
-        'fnlwgt': [fnlwgt],
-        'education': [education],
-        'educational-num': [educational_num],
-        'marital-status': [marital_status],
-        'occupation': [occupation],
-        'relationship': [relationship],
-        'race': [race],
-        'gender': [gender],
-        'capital-gain': [capital_gain],
-        'capital-loss': [capital_loss],
-        'hours-per-week': [hours_per_week],
-        'native-country': [native_country]
-    })
-    prediction = model.predict(data)
-    return {'prediction': prediction[0]}
+# # @app.post("/predict")
+# def predict(age:int,workclass:str,fnlwgt:int,education:str,educational_num:int,marital_status:str,occupation:str,relationship:str,race:str,gender:str,capital_gain:int,capital_loss:int,hours_per_week:int,native_country:str):
+#     data = pd.DataFrame({
+#         'age': [age],
+#         'workclass': [workclass],
+#         'fnlwgt': [fnlwgt],
+#         'education': [education],
+#         'educational-num': [educational_num],
+#         'marital-status': [marital_status],
+#         'occupation': [occupation],
+#         'relationship': [relationship],
+#         'race': [race],
+#         'gender': [gender],
+#         'capital-gain': [capital_gain],
+#         'capital-loss': [capital_loss],
+#         'hours-per-week': [hours_per_week],
+#         'native-country': [native_country]
+#     })
+#     prediction = model.predict(data)
+#     return {'prediction': prediction[0]}
+# prediction = predict(50,"Local-gov",336951,"Assoc-acdm",12,"Married-civ-spouse","Protective-serv","Husband","White","Male",0,0,40,"United-States")
+# print(prediction)
+
